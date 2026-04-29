4a8bb3e4b046f998a67ac53dae24fcb9105c4a36
azumik0720-cmd <azumi.k.0720@gmail.com>
2026-04-29 18:04:02 +0900

    remove config and gitignore

--- config.js
+++
@@ -1,4 +0,0 @@
-// このファイルはGitHubにあげないでください（.gitignoreで除外済み）
-window._SUPABASE_URL = 'YOUR_SUPABASE_URL';
-window._SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
-window._GEMINI_KEY = 'YOUR_GEMINI_API_KEY';
